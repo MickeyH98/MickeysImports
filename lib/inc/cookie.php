20f42c332ded2c10870f2195f81b65bc6af11410
Miguel Hernandez <miguelhernandez@MacBook-Pro.local>
2017-10-12 15:55:15 -0400

    fix cookie

--- lib/inc/cookie.php
+++ lib/inc/cookie.php
@@ -1,6 +1,6 @@
 <?php
 $cookie_name = "reserved";
-$cookie_value = serialize(["Aventador", "911 Carrera"]);
+$cookie_value = serialize([]);
 
 if(!isset($_COOKIE[$cookie_name])){
   setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); //cookie will persist for 1 month
